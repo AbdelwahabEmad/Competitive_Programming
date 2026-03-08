8b0533e8d5bafee7a12633f01a3221824d8b4d4e
AbdelwahabEmad <abd916587@gmail.com>
2026-03-09 01:03:07 +0200

    template OrderedSet

--- Data Structure/orderedSet.cpp
+++ Data Structure/orderedSet.cpp
@@ -10,56 +10,58 @@ using ordered_set = tree<key, null_type, less<key>, rb_tree_tag, tree_order_stat
 
 //zero based
 struct orderedSet {
-    ordered_set<int> se;
+    ordered_set<int> st;
 
     // Add element O(log N)
-    void insert(int val) { se.insert(val); }
+    void insert(int val) {
+        st.insert(val);
+    }
 
     //check existence O(log N)
     bool exist(int val) {
-        return se.find(val) != se.end();
+        return st.find(val) != st.end();
     }
 
     //erase by value O(log N)
     void erase(int val) {
-        auto it = se.find(val);
-        if (it != se.end()) se.erase(it);
+        auto it = st.find(val);
+        if (it != st.end()) st.erase(it);
     }
 
     // return 0-index position O(log N)
     int index(int val) {
         if (!exist(val)) return -1;
-        return se.order_of_key(val);
+        return st.order_of_key(val);
     }
 
     // Returns the value of lower_bound (>= val) O(log N)
     int lower_bound(int val) {
-        auto it = se.lower_bound(val);
-        return (it == se.end() ? -1 : *it);
+        auto it = st.lower_bound(val);
+        return (it == st.end() ? -1 : *it);
     }
 
     // Returns the value of upper_bound (> val) O(log N)
     int upper_bound(int val) {
-        auto it = se.upper_bound(val);
-        return (it == se.end() ? -1 : *it);
+        auto it = st.upper_bound(val);
+        return (it == st.end() ? -1 : *it);
     }
 
     // Access by index O(log N) st[0]
     int operator[](int idx) {
-        if (idx < 0 || idx >= (int) se.size()) return -1;
-        return *se.find_by_order(idx);
+        if (idx < 0 || idx >= (int) st.size()) return -1;
+        return *st.find_by_order(idx);
     }
 
     // Number of elements strictly less than x O(log N)
     int order_of_key(int x) {
-        return se.order_of_key(x);
+        return st.order_of_key(x);
     }
 
     int size() {
-        return se.size();
+        return st.size();
     }
 
     void clear() {
-        se.clear();
+        st.clear();
     }
 };
